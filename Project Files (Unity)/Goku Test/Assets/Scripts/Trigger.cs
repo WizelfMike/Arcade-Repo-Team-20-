569125147ad3e7346f03f135c836c202e56054af
WizelfMike <32264@ma-web.nl>
2021-04-12 19:23:22 +0200

    End Review push

---
+++ Project Files (Unity)/Goku Test/Assets/Scripts/Trigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject Cube;
+
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Ouch");
+    }
+
+    
+    
+}
